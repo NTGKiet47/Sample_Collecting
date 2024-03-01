c1485be496ccf06ca3b0a3aad62f24f06dc648fc
Gia Kiệt <nguyentrangiakiet47@gmail.com>
2024-03-01 15:14:40 +0700

    Added adding projects interface

---
+++ app/src/main/java/com/example/samplecollecting/activity_addProject.kt
@@ -0,0 +1,11 @@
+package com.example.samplecollecting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_addProject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_add_project)
+    }
+}
